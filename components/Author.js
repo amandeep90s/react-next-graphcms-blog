59458bcb9b4e9745e043bad50fb6796121af5b06
amandeep90s <programmeramandeep@gmail.com>
2021-11-06 19:52:32 +0530

    Author details

--- components/Author.js
+++ components/Author.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
-const Author = () => {
-  return <div></div>;
+const Author = ({ author }) => {
+  return (
+    <div className="relative p-12 mt-20 mb-8 text-center bg-black rounded-lg bg-opacity-20">
+      <div className="absolute right-0 left-2 -top-14">
+        <Image
+          src={author.photo.url}
+          unoptimized
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full"
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
+    </div>
+  );
 };
 
 export default Author;
